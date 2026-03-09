29a497a0c8be815a14c79d4f3f65a5417d4d9671
jianfengmao <jianfengmao@deephaven.io>
2026-03-09 13:27:26 -0600

    Update gen_makefiles.ps1

--- cpp-client/deephaven/gen_makefiles.ps1
+++ cpp-client/deephaven/gen_makefiles.ps1
@@ -1,7 +1,6 @@
 cmake -B build -S . `
--DCMAKE_BUILD_TYPE=Debug `
+-DCMAKE_TOOLCHAIN_FILE="$env:VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" `
 -DCMAKE_INSTALL_PREFIX="$env:FLIGHT_SQL_ODBC_INSTALL_DIR" `
 -DCMAKE_PREFIX_PATH="c:/git/arrow/cpp/vcpkg_installed/x64-windows" `
 -DArrow_DIR=C:/flight_sql_odbc_install `
--DX_VCPKG_APPLOCAL_DEPS_INSTALL=ON `
--DCMAKE_TOOLCHAIN_FILE="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/vcpkg/scripts/buildsystems/vcpkg.cmake"
+-DX_VCPKG_APPLOCAL_DEPS_INSTALL=ON
